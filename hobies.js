7ea749b14e0e0dd28ba438b91ac886261adb0d52
Lucas Pirez <lucaspirez42@gmail.com>
2023-03-15 18:26:12 -0300

    refactorizacionDeMejoras

--- hobies.js
+++ hobies.js
@@ -7,71 +7,10 @@ const cocinar = document.getElementById('cocinar_id_description')
 
 const arrHobbies = [lectura, guitarra, musica, series, cocinar]
 
-// const dicSwitch = {
-//   guitarra: (display,index) => {
-//     guitarra.style.display = display
-//     hobbiesBox[index].style.transform = 'scale(1.1)'
-//   }
-// }
-
-function s(display,index,scale)=>{
-  arrHobbies[index].style.display = display;
+function switchDisplay(display, index, scale) {
+  arrHobbies[index].style.display = display
   hobbiesBox[index].style.transform = `scale(${scale})`
 }
-
-// function switchDisplay(val) {
-//   switch (val) {
-//     case 'lectura':
-//       lectura.style.display = 'block'
-//       hobbiesBox[0].style.transform = 'scale(1.1)'
-//       break
-//     case 'guitarra':
-//       guitarra.style.display = 'block'
-//       hobbiesBox[1].style.transform = 'scale(1.1)'
-//       break
-//     case 'musica':
-//       musica.style.display = 'block'
-//       hobbiesBox[2].style.transform = 'scale(1.1)'
-//       break
-//     case 'series':
-//       series.style.display = 'block'
-//       hobbiesBox[3].style.transform = 'scale(1.1)'
-//       break
-//     case 'cocinar':
-//       cocinar.style.display = 'block'
-//       hobbiesBox[4].style.transform = 'scale(1.1)'
-//       break
-//     default:
-//       break
-//   }
-// }
-
-// function switchDisplayNone(val) {
-//   switch (val) {
-//     case 'lectura':
-//       lectura.style.display = 'none'
-//       hobbiesBox[0].style.transform = 'scale(1)'
-//       break
-//     case 'guitarra':
-//       guitarra.style.display = 'none'
-//       hobbiesBox[1].style.transform = 'scale(1)'
-//       break
-//     case 'musica':
-//       musica.style.display = 'none'
-//       hobbiesBox[2].style.transform = 'scale(1)'
-//       break
-//     case 'series':
-//       series.style.display = 'none'
-//       hobbiesBox[3].style.transform = 'scale(1)'
-//       break
-//     case 'cocinar':
-//       cocinar.style.display = 'none'
-//       hobbiesBox[4].style.transform = 'scale(1)'
-//       break
-//     default:
-//       break
-//   }
-// }
 
 let inter
 
@@ -82,12 +21,10 @@ function intervalo() {
       num = 0
     }
     if (num > 0) {
-      // switchDisplayNone(hobbiesBox[num - 1].title)
-      s('none',num-1,1)
+      switchDisplay('none', num - 1, 1)
     }
     if (num < 5) {
-      // switchDisplay(hobbiesBox[num].title)
-      s('block',num,1.1)
+      switchDisplay('block', num, 1.1)
     }
     num++
   }, 3000)
@@ -96,34 +33,29 @@ intervalo()
 
 function clear() {
   clearInterval(inter)
+  arrHobbies.forEach((u) => (u.style.display = 'none'))
   hobbiesBox.forEach((u) => (u.style.transform = 'scale(1)'))
 }
+
 if (window.innerWidth < 768) clear()
 
 window.addEventListener('resize', (e) => {
   if (window.innerWidth < 768) {
     clear()
-    // switchDisplayNone(hobbiesBox[num - 1].title)
-    s('none',num-1,1)
+    switchDisplay('none', num - 1, 1)
   }
 })
 
 hobbiesBox.forEach((box, i) => {
   box.onmouseenter = (e) => {
     const { title } = e.target
-
-    guitarra.style.display = 'none'
-    lectura.style.display = 'none'
-    musica.style.display = 'none'
-    series.style.display = 'none'
-    cocinar.style.display = 'none'
     clear()
-    switchDisplay(title)
+    switchDisplay('block', i, 1.1)
   }
 
   box.onmouseleave = (e) => {
     const { title } = e.target
     intervalo()
-    switchDisplayNone(title)
+    switchDisplay('none', i, 1)
   }
 })
--- modal.js
+++ modal.js
@@ -1,59 +1,57 @@
-const buttomModal = document.getElementsByName("button_modal");
-const modal = document.getElementsByName("modal");
-console.log(buttomModal, modal);
+const buttomModal = document.getElementsByName('button_modal')
+const modal = document.getElementsByName('modal')
 
 function createIcon(bool) {
-  const imgExpand = document.createElement("img");
+  const imgExpand = document.createElement('img')
 
-  if (!bool) imgExpand.src = "assets/expand.svg";
-  else imgExpand.src = "assets/reduce.svg";
+  if (!bool) imgExpand.src = 'assets/expand.svg'
+  else imgExpand.src = 'assets/reduce.svg'
 
-  return imgExpand;
+  return imgExpand
 }
 
 modal.forEach((u, i) => {
-  u.style.transition = "all .3s";
+  u.style.transition = 'all .3s'
 
-  buttomModal[i].title = "expandir";
-  buttomModal[i].appendChild(createIcon(false));
+  buttomModal[i].title = 'expandir'
+  buttomModal[i].appendChild(createIcon(false))
 
   buttomModal[i].onclick = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+    e.preventDefault()
+    e.stopPropagation()
 
-    u.classList.toggle("experiencie__conditional");
-    if (buttomModal[i].title === "expandir") {
-      buttomModal[i].title = "reduce";
+    u.classList.toggle('experiencie__conditional')
+    if (buttomModal[i].title === 'expandir') {
+      buttomModal[i].title = 'reduce'
       buttomModal[i].replaceChild(
         createIcon(true),
-        buttomModal[i].querySelector("img")
-      );
+        buttomModal[i].querySelector('img')
+      )
     } else {
-      buttomModal[i].title = "expandir";
+      buttomModal[i].title = 'expandir'
 
       buttomModal[i].replaceChild(
         createIcon(false),
-        buttomModal[i].querySelector("img")
-      );
+        buttomModal[i].querySelector('img')
+      )
     }
-  };
-});
+  }
+})
 
-window.addEventListener("click", (e) => {
-  e.stopPropagation();
-  console.log("hola");
+window.addEventListener('click', (e) => {
+  e.stopPropagation()
   modal.forEach((u, i) => {
     u.onclick = (e) => {
-      e.stopPropagation();
-    };
-    if ([...u.classList].includes("experiencie__conditional")) {
-      u.classList.remove("experiencie__conditional");
-      buttomModal[i].title = "expandir";
+      e.stopPropagation()
+    }
+    if ([...u.classList].includes('experiencie__conditional')) {
+      u.classList.remove('experiencie__conditional')
+      buttomModal[i].title = 'expandir'
 
       buttomModal[i].replaceChild(
         createIcon(false),
-        buttomModal[i].querySelector("img")
-      );
+        buttomModal[i].querySelector('img')
+      )
     }
-  });
-});
+  })
+})
